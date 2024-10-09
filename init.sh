51b774b072f39d32c722dfadca082b18d9341e1e
Muhammad Afiq <blackzafiqz@gmail.com>
2024-10-09 15:24:53 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# docker
+# https://docs.docker.com/engine/install/ubuntu/
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install containerd.io
+
+# prepare the system
+# https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+sysctl --system
+
+curl "https://raw.githubusercontent.com/tech-k8s/k8s-init/refs/heads/main/config.toml" -o /etc/containerd/config.toml
+systemctl restart containerd
+
+# k8s
+# https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+
+apt install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
